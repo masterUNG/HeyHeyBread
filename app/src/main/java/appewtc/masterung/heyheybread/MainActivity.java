638ed5c6c2ceb6e3ec5af0259da9f744944ac5be
masterUNG <phrombutr@gmail.com>
2016-01-02 11:14:12 +0700

    Delete All SQLite Finish

--- app/src/main/java/appewtc/masterung/heyheybread/MainActivity.java
+++ app/src/main/java/appewtc/masterung/heyheybread/MainActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.heyheybread;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,9 +18,20 @@ public class MainActivity extends AppCompatActivity {
         objManageTABLE = new ManageTABLE(this);
 
         //Test Add New Value
-        testAddValue();
+        //testAddValue();
+
+        //Delete All SQLite
+        deleteAllSQLite();
 
     }   // onCreate
+
+    private void deleteAllSQLite() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
+                MODE_PRIVATE, null);
+        objSqLiteDatabase.delete(ManageTABLE.TABLE_USER, null, null);
+        objSqLiteDatabase.delete(ManageTABLE.TABLE_BREAD, null, null);
+        objSqLiteDatabase.delete(ManageTABLE.TABLE_ORDER, null, null);
+    }
 
     private void testAddValue() {
         objManageTABLE.addNewUser("testUser", "testPass", "testName",
