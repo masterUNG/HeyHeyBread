981f7425cb3c966c203c140affaa70fadccd6f87
masterUNG <phrombutr@gmail.com>
2016-01-03 13:23:43 +0700

    MenuAdapter finish

--- app/src/main/java/appewtc/masterung/heyheybread/MenuAdapter.java
+++ app/src/main/java/appewtc/masterung/heyheybread/MenuAdapter.java
@@ -1,7 +1,70 @@
 package appewtc.masterung.heyheybread;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by masterUNG on 1/3/16 AD.
  */
-public class MenuAdapter {
-}
+public class MenuAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private String[] iconStrings, breadStrings, priceStrings, stockStrings;
+
+    public MenuAdapter(Context objContext, String[] iconStrings, String[] breadStrings, String[] priceStrings, String[] stockStrings) {
+        this.objContext = objContext;
+        this.iconStrings = iconStrings;
+        this.breadStrings = breadStrings;
+        this.priceStrings = priceStrings;
+        this.stockStrings = stockStrings;
+    }   // Constructor
+
+    @Override
+    public int getCount() {
+        return iconStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.my_menu_listview, viewGroup, false);
+
+        //For Image
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imageView7);
+        Picasso.with(objContext)
+                .load(iconStrings[i])
+                .resize(120, 120)
+                .into(iconImageView);
+
+        //For TextView
+        TextView breadTextView = (TextView) objView1.findViewById(R.id.textView12);
+        breadTextView.setText(breadStrings[i]);
+
+        TextView priceTextView = (TextView) objView1.findViewById(R.id.textView15);
+        priceTextView.setText(priceStrings[i]);
+
+        TextView stockTextView = (TextView) objView1.findViewById(R.id.textView16);
+        stockTextView.setText(stockStrings[i]);
+
+        return objView1;
+    }
+}   // Main Class
