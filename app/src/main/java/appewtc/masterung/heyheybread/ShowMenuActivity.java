1daf602f2b04fa9b644d8b22d6da1644cb724f49
masterUNG <phrombutr@gmail.com>
2016-01-03 14:32:17 +0700

    Choose Item Finish

--- app/src/main/java/appewtc/masterung/heyheybread/ShowMenuActivity.java
+++ app/src/main/java/appewtc/masterung/heyheybread/ShowMenuActivity.java
@@ -1,7 +1,11 @@
 package appewtc.masterung.heyheybread;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class ShowMenuActivity extends AppCompatActivity {
@@ -21,8 +25,8 @@ public class ShowMenuActivity extends AppCompatActivity {
         //Setup Value Array
         ManageTABLE objManageTABLE = new ManageTABLE(this);
         String[] iconStrings = objManageTABLE.readAllBread(4);
-        String[] breadStrings = objManageTABLE.readAllBread(1);
-        String[] priceStrings = objManageTABLE.readAllBread(2);
+        final String[] breadStrings = objManageTABLE.readAllBread(1);
+        final String[] priceStrings = objManageTABLE.readAllBread(2);
         String[] stockStrings = objManageTABLE.readAllBread(3);
 
         ListView menuListView = (ListView) findViewById(R.id.listView);
@@ -30,7 +34,41 @@ public class ShowMenuActivity extends AppCompatActivity {
                 iconStrings, breadStrings, priceStrings, stockStrings);
         menuListView.setAdapter(objMenuAdapter);
 
+        //Active When Click ListView
+        menuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                chooseItem(breadStrings[i], priceStrings[i]);
+            }   // event
+        });
+
 
     }   // listViewController
+
+    private void chooseItem(final String breadString, final String priceString) {
+
+        CharSequence[] mySequences = {"1 ชิ้น", "2 ชิ้น", "3 ชิ้น", "4 ชิ้น", "5 ชิ้น",
+                "6 ชิ้น", "7 ชิ้น", "8 ชิ้น", "9 ชิ้น", "10 ชิ้น"};
+
+
+        AlertDialog.Builder objBuilder = new AlertDialog.Builder(this);
+        objBuilder.setTitle(breadString);
+        objBuilder.setSingleChoiceItems(mySequences, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                int intItem = i + 1;
+                updateOrderToSQLite(breadString, priceString, intItem);
+
+                dialogInterface.dismiss();
+            }   // event
+        });
+
+        objBuilder.show();
+
+    }   // chooseItem
+
+    private void updateOrderToSQLite(String breadString, String priceString, int intItem) {
+
+    }   // updateOrderToSQLite
 
 }   // Main Class
