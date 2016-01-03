8e851fe71e5c10523cb95e048b2291fb0f4ce1f2
masterUNG <phrombutr@gmail.com>
2016-01-03 13:02:04 +0700

    Design Menu Listview finish

--- app/src/main/java/appewtc/masterung/heyheybread/HubActivity.java
+++ app/src/main/java/appewtc/masterung/heyheybread/HubActivity.java
@@ -1,13 +1,70 @@
 package appewtc.masterung.heyheybread;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
-public class HubActivity extends AppCompatActivity {
+
+public class HubActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private ImageView orderImageView, readOrderImageView,
+            editImageView, mapImageView, complacencyImageView;
+    private String idString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub);
+
+        //Bind Widget
+        bindWidget();
+
+        //Image Controller
+        imageController();
+
+    } // onCreate
+
+    private void imageController() {
+
+        idString = getIntent().getStringExtra("ID");
+        orderImageView.setOnClickListener(this);
+        readOrderImageView.setOnClickListener(this);
+        editImageView.setOnClickListener(this);
+        mapImageView.setOnClickListener(this);
+        complacencyImageView.setOnClickListener(this);
     }
-}
+
+    private void bindWidget() {
+        orderImageView = (ImageView) findViewById(R.id.imageView2);
+        readOrderImageView = (ImageView) findViewById(R.id.imageView3);
+        editImageView = (ImageView) findViewById(R.id.imageView4);
+        mapImageView = (ImageView) findViewById(R.id.imageView5);
+        complacencyImageView = (ImageView) findViewById(R.id.imageView6);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.imageView2:
+                Intent objIntent = new Intent(HubActivity.this, ShowMenuActivity.class);
+                objIntent.putExtra("ID", idString);
+                startActivity(objIntent);
+                break;
+            case R.id.imageView3:
+                break;
+            case R.id.imageView4:
+                break;
+            case R.id.imageView5:
+                break;
+            case R.id.imageView6:
+                break;
+        }   // switch
+
+
+
+    }   // onClick
+}   // Main Class
---
+++ app/src/main/java/appewtc/masterung/heyheybread/MenuAdapter.java
@@ -0,0 +1,7 @@
+package appewtc.masterung.heyheybread;
+
+/**
+ * Created by masterUNG on 1/3/16 AD.
+ */
+public class MenuAdapter {
+}
