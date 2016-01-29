1e52967a29571d808c6ae5aea51eb8e49f36199c
masterUNG <phrombutr@gmail.com>
2016-01-29 16:37:22 +0700

    Can Click more Button finish

--- app/src/main/java/appewtc/masterung/heyheybread/ConfirmOrderActivity.java
+++ app/src/main/java/appewtc/masterung/heyheybread/ConfirmOrderActivity.java
@@ -4,6 +4,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -32,6 +33,10 @@ public class ConfirmOrderActivity extends AppCompatActivity {
         showView();
 
     }   // Main Method
+
+    public void clickMore(View view) {
+        finish();
+    }
 
     private void showView() {
         dateTextView.setText("วันที่ " + dateString);
